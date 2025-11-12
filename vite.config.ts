5c8958e02d6dd4e03e4ff97fa89ab936825bc53e
MaulikBhimani <maulikbhimani14@gmail.com>
2025-11-12 12:56:19 +0530

    Initial commit - production ready

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/chartered-accountant/',
+  base: "/chartered-accountant/",
   server: {
     host: "::",
     port: 8080,
